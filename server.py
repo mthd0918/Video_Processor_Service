2ee3a1729dc3ba37835abba0abe686bf20020525
Hidechika <mthd0918@gmail.com>
2024-07-18 09:33:16 +0900

    server側のメソッドを整理しました

--- server.py
+++ server.py
@@ -17,63 +17,62 @@ class VideoCompressorServer:
 
         print(f"self.dpath: {self.dpath}")
         
+    def header_protocol(self, connection):
+        # header情報の受け取り
+        print("***header****")
+        header = connection.recv(8)
+        filename_length = int.from_bytes(header[:1], "big")
+        json_length = int.from_bytes(header[1:3], "big")
+        data_length = int.from_bytes(header[4:8], "big")
+        stream_rate = 4096
+        print(f"filename_lenght: {filename_length}")
+        print(f"json_length: {json_length}")
+        print(f"data_lenfth: {data_length}")
+        print(f"sream_rate: {stream_rate}")
+        print("***header****")
+        return filename_length, json_length, data_length, stream_rate
+    
+    def read_client_info(self, connection, filename_length, json_length, data_length):
+        print("***from client***")
+        filename = connection.recv(filename_length).decode('utf-8')
+        print(f"Filename: {filename}")
+        
+        if json_length != 0:
+            raise Exception('JSON data is not currently supported.')
+
+        if data_length == 0:
+            raise Exception('No data to read from client.')
+        
+        print("***from client***")
+        return filename
+    
+    def handle_file_transfer(self, connection, filename, data_length, stream_rate):
+            print(f"***Writing file***")
+            with open(os.path.join(self.dpath, filename), 'wb+') as f:
+                while data_length > 0:
+                    data = connection.recv(min(data_length, stream_rate))
+                    if not data:
+                        print("Connection closed by client")
+                        break
+                    f.write(data)
+                    print(f'received {len(data)} bytes')
+                    data_length -= len(data)
+                    print(f"Remaining data_length: {data_length}")
+            print('Finished downloading the file from client.')
+            print(f"***Writing file***")
+
     def tcp_main(self):
-        while True:
-            # TCP接続の確立
+        while data_length > 0:
             connection, client_address = self.socket.accept()
             try:
                 print('connection from', client_address)
 
-                # ファイルの受信
                 try:
-                    # header情報の受け取り
-                    print("***header****")
-                    header = connection.recv(8)
-                    filename_length = int.from_bytes(header[:1], "big")
-                    json_length = int.from_bytes(header[1:3], "big")
-                    data_length = int.from_bytes(header[4:8], "big")
-                    stream_rate = 4096
-                    print(f"filename_lenght: {filename_length}")
-                    print(f"json_length: {json_length}")
-                    print(f"data_lenfth: {data_length}")
-                    print(f"sream_rate: {stream_rate}")
-                    print("***header****")
-
-                    # クライアントから送られてきた変数の読み取り
-                    print("***from client***")
-                    filename = connection.recv(filename_length).decode('utf-8')
-                    print(f"Filename: {filename}")
-                    # 例外処理
-                    if json_length != 0:
-                        raise Exception('JSON data is not currently supported.')
-
-                    if data_length == 0:
-                        raise Exception('No data to read from client.')
-                    print("***from client***")
-
+                    filename_length, json_length, data_length, stream_rate = self.header_protocol(connection)
+                    filename = self.read_client_info(connection, filename_length, json_length, data_length)
+                    self.handle_file_transfer(connection, filename, data_length, stream_rate)
                 except Exception as e:
-                    print(f'Receiving file error: {e}')
-
-                # 受信したデータをファイルへ書き込み
-                try:
-                    print(f"***Writing file***")
-                    with open(os.path.join(self.dpath, filename),'wb+') as f:
-                        # すべてのデータの読み書きが終了するまで、クライアントから読み込まれます
-                        while data_length > 0:
-                            data = connection.recv(data_length if data_length <= stream_rate else stream_rate)
-                            if not data:  # 接続が閉じられた場合
-                                print("Connection closed by client")
-                                break
-                            f.write(data)
-                            print('recieved {} bytes'.format(len(data)))
-                            data_length -= len(data)
-                            print(data_length)
-
-                    print('Finished downloading the file from client.')
-                    print(f"***Writing file***")
-
-                except Exception as e:
-                    print(f"Writing file error: {e}")
+                    print(f'Error during file transfer: {e}')
 
             except Exception as e:
                 print(f'TCP connection error: {e}')
