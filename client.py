ea4159373b004d418897c18ebf9582335c8b72b9
Hidechika <mthd0918@gmail.com>
2024-07-31 15:24:18 +0900

    mp4ファイルの送信

--- client.py
+++ client.py
@@ -1,65 +1,68 @@
+import json
+import os
 import socket
 import sys
-import os
 
-class VideoCompressorClient:
-    def __init__(self, server_address='0.0.0.0', server_port=9001):
-        self.server_address = server_address
-        self.server_port = server_port
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+MAX_STREAM_RATE = 1400
 
-        print('connecting to {}'.format(server_address, server_port))
+def main():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = '0.0.0.0'
+    server_port = 9001
 
-    def protocol_header(self, filename_length, json_length, data_length):
-        return filename_length.to_bytes(1, "big") + json_length.to_bytes(3,"big") + data_length.to_bytes(4,"big")
+    try:
+        sock.connect((server_address, server_port))
+    except socket.error as err:
+        print(err)
+        sys.exit(1)
 
-    def check_file_size(self, file_path):
-        with open(file_path, 'rb') as f:
-            f.seek(0, os.SEEK_END)
-            filesize = f.tell()
-            f.seek(0, 0)
-        if filesize > pow(2, 32):
-            raise ValueError('File must be below 2GB')
-        return filesize
+    # filepath = input("Enter upload filepath: ")
+    filepath = 'media/20719215-uhd_3840_2160_60fps.mp4'
 
-    def send_header(self, filename, filesize):
-        filename_bits = filename.encode('utf-8')
-        header = self.protocol_header(len(filename_bits), 0, filesize)
-        self.socket.send(header)
-        self.socket.send(filename_bits)
+    with open(filepath, 'rb') as f:
+        f.seek(0, os.SEEK_END)
+        filesize = f.tell()
+        f.seek(0,0)
 
-    def send_file_content(self, file_path):
-        with open(file_path, 'rb') as f:
-            while True:
-                data = f.read(4096)
-                if not data:
-                    break
-                print('Sending file data')
-                self.socket.send(data)
+        if filesize > pow(2,32):
+            raise Exception('File must be below 4GB.')
 
-    def tcp_main(self):
-        try:
-            self.socket.connect((self.server_address, self.server_port))
-            
-            file_path = input("Enter file name: ")
-            filename = os.path.basename(file_path)
-            
-            filesize = self.check_file_size(file_path)
-            self.send_header(filename, filesize)
-            self.send_file_content(file_path)
+        # command = input("Choose command: ")
 
-        except FileNotFoundError as e:
-            print(f"File error: {e}")
-        except ValueError as e:
-            print(f"File size error: {e}")
-        except ConnectionError as e:
-            print(f"Connection error: {e}")
-        except Exception as e:
-            print(f"Unexpected error while sending file: {e}")
-        finally:
-            print('closing socket')
-            self.socket.close()
+        filename = os.path.basename(f.name)
+        media_type = os.path.splitext(filename)[1]
+
+        file_info = {
+            'filename': filename
+            # 'command': command
+        }
+        json_data = json.dumps(file_info)
+
+        json_length = len(json_data).to_bytes(2, "big")
+        media_type_size = len(media_type).to_bytes(1, "big")
+        payload_size = filesize.to_bytes(5, "big")
+
+
+        header = json_length + media_type_size + payload_size
+        
+        json_data_bytes = json_data.encode('utf-8')
+        media_type_bytes = media_type.encode('utf-8')
+
+        body = json_data_bytes + media_type_bytes
+
+        data = header + body
+
+        sock.send(data)
+
+        print(data)
+
+        payload = f.read(MAX_STREAM_RATE)
+        while payload:
+            print("Sending...")
+            sock.send(payload)
+            payload = f.read(MAX_STREAM_RATE)
+
+        sock.send(json_data.encode('utf-8'))
 
 if __name__ == '__main__':
-    client = VideoCompressorClient()
-    client.tcp_main()
+    main()
--- server.py
+++ server.py
@@ -1,95 +1,53 @@
+import json
 import socket
 import os
-from pathlib import Path
 
-"""""
-# todo
-## 目標: MP4ファイルをCLIを使ってアップロード
-### 要件
-- すべてのデータが送信されるまで、最大サイズとして 1400 バイトのパケットを使用
-- サーバーへ最初に送信される32バイト -> ファイルのバイト数を通知
-- 4GB(2^32)までのファイルに対応
-- サーバーは受け取ったファイルをmp4ファイルとして解釈、クライアントは送信するファイルがmp4ファイルかどうかチェック
-- レスポンスコードで応答、16バイトのメッセージ
-"""""
+MAX_STREAM_RATE = 1400
 
-class VideoCompressorServer:
-    def __init__(self, server_address='0.0.0.0', server_port=9001):
-        self.server_address = server_address
-        self.server_port = server_port
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.bind((self.server_address, self.server_port))
-        self.socket.listen(1)
-        print('Starting up on {} port {}'.format(server_address, server_port))
+def main():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = '0.0.0.0'
+    server_port = 9001
+    sock.bind((server_address, server_port))
+    sock.listen(1)
 
-        self.dpath = 'output/'
-        if not os.path.exists(self.dpath):
-            os.makedirs(self.dpath)
+    dpath = 'output/'
+    if not os.path.exists(dpath):
+        os.makedirs(dpath)
 
-        print(f"self.dpath: {self.dpath}")
-
-    def read_header(self, connection):
-        print("***header****")
+    while True:
+        print("Starting server...")
+        connection, _ = sock.accept()
+        
+        # headerの受信
         header = connection.recv(8)
-        filename_length = int.from_bytes(header[:1], "big")
-        json_length = int.from_bytes(header[1:3], "big")
-        data_length = int.from_bytes(header[4:8], "big")
-        stream_rate = 4096
-        print(f"filename_lenght: {filename_length}")
-        print(f"json_length: {json_length}")
-        print(f"data_lenfth: {data_length}")
-        print(f"sream_rate: {stream_rate}")
-        print("***header****")
-        return filename_length, json_length, data_length, stream_rate
+        json_length = int.from_bytes(header[:2], "big")
+        media_type_size = int.from_bytes(header[2:3], "big")
+        payload_size = int.from_bytes(header[3:], "big")
 
-    def read_filename(self, connection, filename_length, json_length, data_length):
-        print("***from client***")
-        filename = connection.recv(filename_length).decode('utf-8')
-        print(f"Filename: {filename}")
+        print("json_length:", json_length)
+        print("media_type_size:", media_type_size)
+        print("payload_size:", payload_size)
+
+        # jsonデータの受信
+        json_data = connection.recv(json_length)
+        json_obj = json.loads(json_data.decode('utf-8'))
+        filename = json_obj['filename']
         
-        if json_length != 0:
-            raise Exception('JSON data is not currently supported.')
+        print("json obj: ", json_obj)
 
-        if data_length == 0:
-            raise Exception('No data to read from client.')
-        
-        print("***from client***")
-        return filename
-    
-    def handle_file_transfer(self, connection, filename, data_length, stream_rate):
-            print(f"***Writing file***")
-            with open(os.path.join(self.dpath, filename), 'wb+') as f:
-                while data_length > 0:
-                    data = connection.recv(min(data_length, stream_rate))
-                    if not data:
-                        print("Connection closed by client")
-                        break
-                    f.write(data)
-                    print(f'received {len(data)} bytes')
-                    data_length -= len(data)
-                    print(f"Remaining data_length: {data_length}")
-            print('Finished downloading the file from client.')
-            print(f"***Writing file***")
+        # media_typeの受信
+        media_type = connection.recv(media_type_size)
 
-    def tcp_main(self):
-        while True:
-            connection, client_address = self.socket.accept()
-            try:
-                print('connection from', client_address)
+        print("media_type: ",media_type)
 
-                try:
-                    filename_length, json_length, data_length, stream_rate = self.read_header(connection)
-                    filename = self.read_filename(connection, filename_length, json_length, data_length)
-                    self.handle_file_transfer(connection, filename, data_length, stream_rate)
-                except Exception as e:
-                    print(f'Error during file transfer: {e}')
-
-            except Exception as e:
-                print(f'TCP connection error: {e}')
-            finally:
-                print("Closing current connection")
-                connection.close()
+        with open(os.path.join(dpath, filename), 'wb+') as f:
+            while payload_size > 0:
+                payload = connection.recv(payload_size if payload_size <= MAX_STREAM_RATE else MAX_STREAM_RATE)
+                print('recieved {} bytes'.format(len(payload)))
+                f.write(payload)
+                payload_size -= len(payload)
+                print(payload_size)
 
 if __name__ == '__main__':
-    server = VideoCompressorServer()
-    server.tcp_main()
+    main()
