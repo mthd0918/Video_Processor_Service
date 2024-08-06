57e8d8ac00425dc1dac6d0a5d02fe6f902f8f0db
Hidechika <mthd0918@gmail.com>
2024-08-06 17:26:29 +0900

    クラス化

--- client.py
+++ client.py
@@ -3,106 +3,114 @@ import os
 import socket
 import sys
 
-MAX_STREAM_RATE = 1400
+class Client:
+    MAX_STREAM_RATE = 1400
 
-COMMAND_LIST = {
-    1: {'command': 'compress_video', 'discription': 'Compress video'},
-    2: {'command': 'resize_video', 'discription': 'Change video resolution'},
-    3: {'command': 'change_aspect_ratio', 'discription': 'Modify video aspect ratio'},
-    4: {'command': 'extract_audio', 'discription': 'Convert video to audio'},
-    5: {'command': 'create_gif', 'discription': 'Create GIF from specified time range'},
-}
+    COMMAND_LIST = {
+        1: {'command': 'compress_video', 'description': 'Compress video'},
+        2: {'command': 'resize_video', 'description': 'Change video resolution'},
+        3: {'command': 'change_aspect_ratio', 'description': 'Modify video aspect ratio'},
+        4: {'command': 'extract_audio', 'description': 'Convert video to audio'},
+        5: {'command': 'create_gif', 'description': 'Create GIF from specified time range'},
+    }
 
-COMMAND_PARAMS = {
-    'compress_video': [],
-    'resize_video': ['width', 'height'],
-    'change_aspect_ratio': ['aspect_ratio'],
-    'extract_audio': [],
-    'create_gif': ['start_time', 'duration'],
-}
+    COMMAND_PARAMS = {
+        'compress_video': [],
+        'resize_video': ['width', 'height'],
+        'change_aspect_ratio': ['aspect_ratio'],
+        'extract_audio': [],
+        'create_gif': ['start_time', 'duration'],
+    }
 
-def input_command(command_list):
-    print("************************")
-    print("Enter a number...")
-    for num, data in command_list.items():
-        print(f"{num}: {data['discription']}")
-    print("************************")
+    def __init__(self, server_address, server_port):
+        self.server_address = server_address
+        self.server_port = server_port
+        self.sock = None
+
+    def connect(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            self.sock.connect((self.server_address, self.server_port))
+        except socket.error as err:
+            print(err)
+            sys.exit(1)
     
-    while True:
-        command_num = int(input("Choose command:"))
-        if command_num in command_list:
-            return command_list[command_num]['command']
-        else:
-            print("Invalid value, choose command 1~5\n")
+    def input_command(self):
+        print("************************")
+        print("Enter a number...")
+        for num, data in self.COMMAND_LIST.items():
+            print(f"{num}: {data['description']}")
+        print("************************")
+        
+        while True:
+            command_num = int(input("Choose command:"))
+            if command_num in self.COMMAND_LIST:
+                return self.COMMAND_LIST[command_num]['command']
+            else:
+                print("Invalid value, choose command 1~5\n")
 
-def get_command_params(command):
-    params = {}
-    for param in COMMAND_PARAMS[command]:
-        if param in ['width', 'height']:
-            params[param] = input(f"Enter {param} (pixels): ")
-        elif param == 'aspect_ratio':
-            params[param] = input("Enter aspect ratio (e.g., 16:9): ")
-        elif param == 'start_time':
-            params[param] = input("Enter start time (format: HH:MM:SS): ")
-        elif param == 'duration':
-            params[param] = input("Enter duration (seconds): ")
-    return params
+    def get_command_params(self, command):
+        params = {}
+        for param in self.COMMAND_PARAMS[command]:
+            if param in ['width', 'height']:
+                params[param] = input(f"Enter {param} (pixels): ")
+            elif param == 'aspect_ratio':
+                params[param] = input("Enter aspect ratio (e.g., 16:9): ")
+            elif param == 'start_time':
+                params[param] = input("Enter start time (format: HH:MM:SS): ")
+            elif param == 'duration':
+                params[param] = input("Enter duration (seconds): ")
+        return params
+
+    def send_file(self, filepath):
+        with open(filepath, 'rb') as f:
+            f.seek(0, os.SEEK_END)
+            filesize = f.tell()
+            f.seek(0,0)
+
+            if filesize > pow(2,32):
+                raise Exception('File must be below 4GB.')
+
+            full_filename = os.path.basename(f.name)
+            filename, media_type = os.path.splitext(full_filename)
+
+            print("filename:", filename)
+            print("media_type:", media_type)
+
+            command = self.input_command()
+            params = self.get_command_params(command)
+
+            file_info = {
+                'filename': filename,
+                'command': command,
+                **params
+            }
+            json_data = json.dumps(file_info)
+
+            json_length = len(json_data).to_bytes(2, "big")
+            media_type_size = len(media_type).to_bytes(1, "big")
+            payload_size = filesize.to_bytes(5, "big")
+
+            json_data_bytes = json_data.encode('utf-8')
+            media_type_bytes = media_type.encode('utf-8')
+
+            header = json_length + media_type_size + payload_size
+            data = header + json_data_bytes + media_type_bytes
+            self.sock.send(data)
+
+            payload = f.read(self.MAX_STREAM_RATE)
+            while payload:
+                self.sock.send(payload)
+                payload = f.read(self.MAX_STREAM_RATE)
+
+            self.sock.send(json_data.encode('utf-8'))
 
 def main():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_address = '0.0.0.0'
-    server_port = 9001
-
-    try:
-        sock.connect((server_address, server_port))
-    except socket.error as err:
-        print(err)
-        sys.exit(1)
-
-    # filepath = input("Enter upload filepath: ")
-    filepath = 'media/free-video1-sea-cafinet.mp4'                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
-
-    with open(filepath, 'rb') as f:
-        f.seek(0, os.SEEK_END)
-        filesize = f.tell()
-        f.seek(0,0)
-
-        if filesize > pow(2,32):
-            raise Exception('File must be below 4GB.')
-
-        full_filename = os.path.basename(f.name)
-        filename, media_type = os.path.splitext(full_filename)
-
-        print("filename:", filename)
-        print("media_type:", media_type)
-
-        command = input_command(COMMAND_LIST)
-        params = get_command_params(command)
-
-        file_info = {
-            'filename': filename,
-            'command': command,
-            **params
-        }
-        json_data = json.dumps(file_info)
-
-        json_length = len(json_data).to_bytes(2, "big")
-        media_type_size = len(media_type).to_bytes(1, "big")
-        payload_size = filesize.to_bytes(5, "big")
-
-        json_data_bytes = json_data.encode('utf-8')
-        media_type_bytes = media_type.encode('utf-8')
-
-        header = json_length + media_type_size + payload_size
-        data = header + json_data_bytes + media_type_bytes
-        sock.send(data)
-
-        payload = f.read(MAX_STREAM_RATE)
-        while payload:
-            sock.send(payload)
-            payload = f.read(MAX_STREAM_RATE)
-
-        sock.send(json_data.encode('utf-8'))
+    client = Client('0.0.0.0', 9001)
+    client.connect()
+    # filepath = input("Enter filepath to your video")
+    filepath = 'media/free-video1-sea-cafinet.mp4'
+    client.send_file(filepath)
 
 if __name__ == '__main__':
     main()
--- server.py
+++ server.py
@@ -4,66 +4,50 @@ import os
 import subprocess
 from pathlib import Path
 
-MAX_STREAM_RATE = 1400
+class VideoServer:
+    MAX_STREAM_RATE = 1400
 
-COMMAND_PARAMS = {
-    'compress_video': [],
-    'resize_video': ['width', 'height'],
-    'change_aspect_ratio': ['aspect_ratio'],
-    'extract_audio': [],
-    'create_gif': ['start_time', 'duration'],
-}
+    COMMAND_PARAMS = {
+        'compress_video': [],
+        'resize_video': ['width', 'height'],
+        'change_aspect_ratio': ['aspect_ratio'],
+        'extract_audio': [],
+        'create_gif': ['start_time', 'duration'],
+    }
 
-ffmpeg_commands = {
-    "compress_video": "ffmpeg -i {input} -c:v libx264 -crf 23 {output}",
-    "resize_video": "ffmpeg -i {input} -vf scale={width}:{height} -c:a copy {output}",
-    "change_aspect_ratio": "ffmpeg -i {input} -aspect {aspect_ratio} -c:a copy {output}",
-    "extract_audio": "ffmpeg -i {input} -vn -acodec libmp3lame {output}",
-    "create_gif": "ffmpeg -i {input} -ss {start_time} -t {duration} -vf \"fps=10,scale=320:-1:flags=lanczos\" {output}",
-}
+    FFMPEG_COMMANDS = {
+        "compress_video": "ffmpeg -i {input} -c:v libx264 -crf 23 {output}",
+        "resize_video": "ffmpeg -i {input} -vf scale={width}:{height} -c:a copy {output}",
+        "change_aspect_ratio": "ffmpeg -i {input} -aspect {aspect_ratio} -c:a copy {output}",
+        "extract_audio": "ffmpeg -i {input} -vn -acodec libmp3lame {output}",
+        "create_gif": "ffmpeg -i {input} -ss {start_time} -t {duration} -vf \"fps=10,scale=320:-1:flags=lanczos\" {output}",
+    }
 
-def ffmpeg_process(command_key, input_file, output_file, params):
-    if command_key not in ffmpeg_commands:
-        raise ValueError(f"Unknown command key: {command_key}")
-    
-    command = ffmpeg_commands[command_key]
-    format_params = {'input': input_file, 'output': output_file}
+    def __init__(self, server_address, server_port):
+        self.server_address = server_address
+        self.server_port = server_port
+        self.sock = None
 
-    # 各コマンド特有のパラメーターを追加
-    for param in COMMAND_PARAMS[command_key]:
-        if param in params:
-            format_params[param] = params[param]
-    
-    execute_command = command.format(**format_params)
-    
-    try:
-        subprocess.run(execute_command, shell=True, check=True)
-        print(f"Command executed successfully: {execute_command}")
-    except subprocess.CalledProcessError as e:
-        print(f"Error executing command: {e}")
+    def start(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind((self.server_address, self.server_port))
+        self.sock.listen(1)
 
-def main():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_address = '0.0.0.0'
-    server_port = 9001
-    sock.bind((server_address, server_port))
-    sock.listen(1)
+        dpath = 'uploaded_file/'
+        if not os.path.exists(dpath):
+            os.makedirs(dpath)
 
-    dpath = 'uploaded_file/'
-    if not os.path.exists(dpath):
-        os.makedirs(dpath)
+        while True:
+            print("Starting server...")
+            connection, _ = self.sock.accept()
+            self.handle_client(connection)
 
-    while True:
-        print("Starting server...")
-        connection, _ = sock.accept()
-        
-        # headerの受信
+    def handle_client(self, connection):
         header = connection.recv(8)
         json_length = int.from_bytes(header[:2], "big")
         media_type_size = int.from_bytes(header[2:3], "big")
         payload_size = int.from_bytes(header[3:], "big")
 
-        # json_data, media_type, payloadの受信
         data = connection.recv(json_length + media_type_size)
         json_data = data[:json_length]
         media_type = data[json_length:].decode('utf-8')
@@ -72,15 +56,14 @@ def main():
         filename = json_obj['filename']
         command = json_obj['command']
 
+        dpath = 'uploaded_file/'
         with open(os.path.join(dpath, filename + media_type), 'wb+') as f:
             while payload_size > 0:
-                payload = connection.recv(payload_size if payload_size <= MAX_STREAM_RATE else MAX_STREAM_RATE)
+                payload = connection.recv(payload_size if payload_size <= self.MAX_STREAM_RATE else self.MAX_STREAM_RATE)
                 f.write(payload)
                 payload_size -= len(payload)
 
-
-        # ffmpegの処理
-        params = {key: json_obj.get(key) for key in COMMAND_PARAMS[command]}
+        params = {key: json_obj.get(key) for key in self.COMMAND_PARAMS[command]}
         input_filename = f"{filename}{media_type}"
 
         if command == 'extract_audio':
@@ -88,15 +71,37 @@ def main():
         elif command == 'create_gif':
             media_type ='.gif'   
 
-        output_filename = f"processed_{filename}{media_type}"
+        output_filename = f"{command}_processed_{filename}{media_type}"
 
         input_file = Path("uploaded_file") / input_filename
         output_file = Path("processed_file") / output_filename
 
-        ffmpeg_process(command, input_file, output_file, params)
+        self.ffmpeg_process(command, input_file, output_file, params)
 
-        # アップロードされた動画の削除
         os.remove(input_file)
+
+    def ffmpeg_process(self, command_key, input_file, output_file, params):
+        if command_key not in self.FFMPEG_COMMANDS:
+            raise ValueError(f"Unknown command key: {command_key}")
+        
+        command = self.FFMPEG_COMMANDS[command_key]
+        format_params = {'input': input_file, 'output': output_file}
+
+        for param in self.COMMAND_PARAMS[command_key]:
+            if param in params:
+                format_params[param] = params[param]
+        
+        execute_command = command.format(**format_params)
+        
+        try:
+            subprocess.run(execute_command, shell=True, check=True)
+            print(f"Command executed successfully: {execute_command}")
+        except subprocess.CalledProcessError as e:
+            print(f"Error executing command: {e}")
+
+def main():
+    server = VideoServer('0.0.0.0', 9001)
+    server.start()
 
 if __name__ == '__main__':
     main()
